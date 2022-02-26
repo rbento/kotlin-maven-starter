b33219830e749eee1e988e3bc5f5c59273d0231f
Rodrigo Bento <rodrigo.bento@gmail.com>
2022-02-26 18:30:39 -0500

    Add Kotlin src

---
+++ src/main/kotlin/rbento/Main.kt
@@ -0,0 +1,7 @@
+package rbento
+
+import java.time.LocalDate
+
+fun main() {
+	print("(c) ${LocalDate.now().year}, Rodrigo Bento")
+}
---
+++ src/test/kotlin/rbento/Main.kt
@@ -0,0 +1,10 @@
+package rbento
+
+import org.junit.jupiter.api.Test
+
+class Main {
+
+	@Test
+	fun testMain() {
+	}
+}
